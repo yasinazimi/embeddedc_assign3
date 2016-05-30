9dbdf6e3e6e10b05e33ed760c333bf642e8c4da3
Yasin Azimi <yasin_azimi@live.com>
2016-05-30 23:08:42 +1000

    linkedlist and delhead working

--- advassignment.c
+++ advassignment.c
@@ -2,7 +2,7 @@
 48430 Embedded C - Advanced Assignment
 Name: Yasin Azimi
 Student ID: 11733490
-Date of submission: 23/05/2016
+Date of submission: 30/05/2016
 A brief statement on what you could achieve (less than 50 words):
 = 
 
@@ -20,46 +20,67 @@ List of header files and preprocessing directives
 
 
 /**************************************************************************
-Structs:
+Structs
 ***************************************************************************/
 struct node{
 	char c;
-	struct node* next;
+	struct node *next;
 };
 
-typedef struct node point;
+typedef struct node node_n;
 
 
 /**************************************************************************
 Function prototypes
 ***************************************************************************/
+int main(int argc, char* argv[]);
 void printmenu(void);
-void printlinkedlist();
-int delhead();
-int delvowels(char vowel);
+void printlinkedlist(node_n*first);
+int delhead(node_n**first);
+int delvowels(node_n**first);
+int isvowel(char v);
 
 
 /**************************************************************************
 Main
 ***************************************************************************/
-int main() {
-	int vowel = 0;
+int main(int argc, char* argv[]) {
+	int m, n;
 	char option;
+	while (argc <= 1) {
+		printf("Parse a sentence");
+		return 1;
+	}
+	
+	node_n* first = NULL;
+	first = malloc(sizeof(node_n));
+	if (first == NULL) {
+		return 1;
+	}
+	
+	node_n* curr = first;
+	for (m = 1; m < argc; m++) {
+		for (n = 0; argv[m][n] != '\0'; n++) {
+			curr->c = argv[m][n];
+			curr->next = malloc(sizeof(node_n));
+			curr = curr->next;
+		}
+	} curr->next = NULL;
 	
 	do {
 		printmenu();
 		printf("Enter your choice>");
 		scanf("%c", &option);
-		switch (option)
-		{
-			case '1': printlinkedlist(); break;
-			case '2': delhead(); break;
-			case '3': delvowels(vowel); break;
-			case '4': exit(0); break;
-			default: exit(EXIT_SUCCESS);
+		switch (option) {
+			case '1': printlinkedlist(first); break;
+			case '2': delhead(&first); break;
+			case '3': break;
+			case '4': exit(0);
+			default: printf("Invalid choice.\n"); break;
 		}
-	} while (option != '4');
-	return 1;
+		while (getchar() != '\n');
+	} while (option != 4);
+	return 0;
 }
 
 
@@ -76,36 +97,66 @@ void printmenu(void){
 
 
 /**************************************************************************
-Print all nodes in a linked list of struct node type. Follow the print
-format of the executable provided. Pass ONLY ONE PARAMETER to this
-function ("Parse a sentence")
+Print all nodes in a linked list of struct node type
 ***************************************************************************/
-void printlinkedlist(point**ptr){
-	char c;
-	c = getchar();
-	printf("Parse a sentence> ");
-	while((c = getchar()) != '\n') {
-		putchar(c);
-	}	
+void printlinkedlist(node_n*first) {
+	node_n* curr = first;
+	do {
+		printf("%c", curr->c);
+		curr = curr->next;
+	} while (curr != NULL);
+	printf("\n");
 }
 
 
 /**************************************************************************
-Delete the head (first node) of a linked list of type struct node. Pass
-ONLY ONE PARAMETER to this function.
-return 0 - if the head of the linked list could be deleted successfully
-return 1 - otherwise
+Delete the head (first node) of a linked list of type struct node
 ***************************************************************************/
-int delhead(){
-	return 0;
+int delhead(node_n**first) {
+	node_n* next_node = NULL;
+	if (*first != NULL) {
+		next_node = (*first)->next;
+		free(*first);
+		*first = next_node;
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
 
 
 /**************************************************************************
 Delete the vowels (a,e,i,o,u) in a sentence
 ***************************************************************************/
-int delvowels(char vowel){
-	switch(vowel) {
+int delvowels(node_n**first) {
+	int isvowel(char v);
+	node_n *resetfirst = 0;
+	*first = resetfirst;
+	
+	while (first != 0) {
+		if(isvowel(*first->c) == 1) {
+			first->c = 1;
+		}
+		first = first->next;
+	}
+	*first = resetfirst;
+	
+	while (first != 0) {
+		if (first->c != 1) {
+			printf("%c", first->c);
+		}
+		first = first->next;
+	}
+	
+}
+
+
+/**************************************************************************
+Delete the vowels (a,e,i,o,u) in a sentence
+***************************************************************************/
+int isvowel(char v) {
+	switch(v) {
 		case 'a': return 1;
 		case 'e': return 1;
 		case 'i': return 1;
